60daf18d2d58c99dc10fb9a4e9a1ae29419359c6
Sergio Dominguez <sergiodominguez21sdd@gmail.com>
2023-07-04 02:51:15 -0300

    terminamos registro paso a paso , pero persiste una falla al cargar ususarios. no se cargan en el db.json

--- src/componentes/registro/RegisterUsers.jsx
+++ src/componentes/registro/RegisterUsers.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react"
+import axios from "axios"
+
+
 
 const RegisterUsers = () => {
 
@@ -11,35 +14,60 @@ const RegisterUsers = () => {
     })
 
 
+    const handleChange = (e) => {  //con esta funcion actualizaremos dataUsery setdataUser- aqui tomamos datos del formulario/actualizamos los valores del estado con los inputs
+        setdataUser({ ...dataUser, [e.target.name]: e.target.value })
+    }
+
+
+
+
+    const handleSubmit = (e) => { //poner datos en la fake api
+        e.preventDeFaultt()
+        try {
+            const response = axios.post("http://localhost:8080/users", dataUser)
+            console.log(response);
+            setdataUser({
+                nombre: "", 
+                apellido: "",
+                edad: "",
+                password: "",
+            });
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
 
     return (
 
         <>
-        <form action="">
+            <form onSubmit={handleSubmit}>
 
-            <div className="mb-3">
-                <label htmlFor="nombre" className="form-label">Nombre</label>
-                <input type="text" className="form-control" id="nombre" aria-aria-describedby="nombre" />
+                <div className="mb-3">
+                    <label htmlFor="nombre" className="form-label">Nombre</label>
+                    <input type="text" className="form-control" value={dataUser.nombre} onChange={handleChange} name="nombre" aria-aria-describedby="nombre" />
 
-            </div>
-            <div className="mb-3">
-                <label htmlFor="apellido" className="form-label">Apellido</label>
-                <input type="text" className="form-control" id="apellido" aria-aria-describedby="apellido" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="apellido" className="form-label">Apellido</label>
+                    <input type="text" className="form-control" value={dataUser.apellido} onChange={handleChange} name="apellido" aria-aria-describedby="apellido" />
 
-            </div>
-            <div className="mb-3">
-                <label htmlFor="edad" className="form-label">Edad</label>
-                <input type="text" className="form-control" id="edad" aria-aria-describedby="edad" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="edad" className="form-label">Edad</label>
+                    <input type="text" className="form-control" value={dataUser.edad} onChange={handleChange} name="edad" aria-aria-describedby="edad" />
 
-            </div>
-            <div className="mb-3">
-                <label htmlFor="password" className="form-label">Contraseña</label>
-                <input type="text" className="form-control" id="password" aria-aria-describedby="password" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Contraseña</label>
+                    <input type="text" className="form-control" value={dataUser.password} onChange={handleChange} name="password" aria-aria-describedby="password" />
 
-            </div>
-            <button type="submit" className="btn btn-primary">Enviar</button>
+                </div>
+                <button type="submit" className="btn btn-primary">Enviar</button>
 
-        </form>
+            </form>
 
         </>
 
