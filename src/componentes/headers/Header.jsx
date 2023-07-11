c9d3d5a4730227f4571643bd54de49d5a89ecdd9
Sergio Dominguez <sergiodominguez21sdd@gmail.com>
2023-07-10 23:54:46 -0300

    ediciones varias

--- src/componentes/headers/Header.jsx
+++ src/componentes/headers/Header.jsx
@@ -15,17 +15,17 @@ const Header =() => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#home">Inicio</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <Nav.Link href="#home">Iniciar sesión</Nav.Link>
+            <Nav.Link href="#RegisterUsers">Registrate</Nav.Link>
+            <NavDropdown title="MENU" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Catalogo</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-                Another action
+                Productos
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Ofertas</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
-                Separated link
+                Trabaja Con Nosotros
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
